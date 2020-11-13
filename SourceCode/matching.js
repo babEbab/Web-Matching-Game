761a158c6e95ecfa6e0598d0ac3aaee0e86beea6
babebab <dasomchoi52@gmail.com>
2020-11-14 02:43:22 +0900

    이미지와 소스 코드 수정
    이미지 수정: 2장의 이미지의 비율 조정
    소스 코드 수정: matching.js의 flip 함수 관련, gameStyle.css의 버튼 사이즈 관련

--- SourceCode/matching.js
+++ SourceCode/matching.js
@@ -1,13 +1,14 @@
 window.onload = start;
 
 var card = new Array(16); // 카드 배열
-var cnt = 0; // 플레이어가 카드를 뒤집은 횟수(2가 되면 서로 같은 카드인지 확인)
-var selectCard = new Array(2); // 사용자가 현재 선택한 카드 배열(1번에 최대 2장의 앞면을 보는 것이 가능)
-var clickCnt = 0; // 플레이어가 카드를 뒤집은 총 횟수(2회 클릭 시에 1 증가)
 
 var catImage = new Array("../Image/Cat/01.jpg", "../Image/Cat/01.jpg","../Image/Cat/02.jpg", "../Image/Cat/02.jpg","../Image/Cat/03.jpg", "../Image/Cat/03.jpg","../Image/Cat/04.jpg", "../Image/Cat/04.jpg","../Image/Cat/05.jpg", "../Image/Cat/05.jpg","../Image/Cat/06.jpg", "../Image/Cat/06.jpg","../Image/Cat/07.jpg", "../Image/Cat/07.jpg","../Image/Cat/08.jpg", "../Image/Cat/08.jpg");
-
 var catImageFlag = new Array("0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0");
+// var flipCard =  new Array("0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0");
+
+var cnt = 0; // 플레이어가 카드를 뒤집은 횟수(2가 되면 서로 같은 카드인지 확인)
+var selectCard = new Array(2); // 사용자가 현재 선택한 카드 배열(1번에 최대 2장의 앞면을 보는 것이 가능)
+var nowClickCount = 0; // 플레이어가 카드를 뒤집은 총 횟수(2회 클릭 시에 1 증가)
 
 // 시작 함수
 function start(){
@@ -18,7 +19,7 @@ function start(){
     playTable.style.marginLeft="auto";
     playTable.style.marginRight="auto";
     playBoard.appendChild(playTable);
-    
+
     for(var i = 0; i < card.length; i++){
         if((i % 4) == 0){
             var tr = document.createElement("tr");
@@ -49,23 +50,76 @@ function shuffle(){
                break;
             }
         }
-    }      
+    }
 }
 
 function flip(obj){
-    obj.src = obj.cat;
+    if(obj.flipFlag == 1) return;
+    if(cnt != 0){
+        if(selectCard[0].id==obj.id) return;
+        //else if(selectCard[1].id==obj.id) return;
+    }
     selectCard[cnt] = obj;
+    obj.src = obj.cat;
     cnt++; // cnt 1 증가
     if(cnt == 2){
-        if(selectCard[0].src != selectCard[1].src){
+        if(selectCard[0].src != selectCard[1].src){ // 한 세트의 짝맞추기를 실패하면 해당 카드는 다시 뒷면으로 돌아간다
             selectCard[0].src = "../Image/Cat/backCat.jpg";
             selectCard[1].src = "../Image/Cat/backCat.jpg";
         }
-        clickCnt++; // clickCnt 1 증가
+        else{ // 한 세트의 짝을 맞추면 해당 카드의 flipFlag는 1이 된다
+            selectCard[0].flipFlag = 1;
+            selectCard[1].flipFlag = 1;
+            /* 짝맞추기가 완료되었는지 확인하는 코드 */
+            var flag = true;
+            for(var i=0;i<card.length;i++){
+                if(card[i].flipFlag == 0){
+                    flag = false;
+                }
+            }
+            if (flag == true){
+                clear(); // 짝맞추기 완료
+            }
+        }
+        nowClickCount++; // nowClickCnt 1 증가
+        document.querySelector("#clickCnt").value = nowClickCount; // nowClickCnt를 html의 clickCnt에 반영
         cnt = 0; // cnt 초기화
     }
 }
 
+function clear(){
+    confirm("모든 짝을 맞추셨습니다. 게임을 재시작하세요!");
+    // 현재 기록을 로컬 스토리지에 추가...
+    restart();
+}
+
+
+
+// function flip(obj){
+//     if(obj.flipFlag == 1) return;
+//     obj.src = obj.cat;
+//     if(nowClickCount>0){
+//         if(selectCard[cnt].id==obj.id);
+//     }
+//     selectCard[cnt] = obj;
+//     cnt++; // cnt 1 증가
+//     if(cnt == 2){
+//         if(selectCard[0].src != selectCard[1].src){
+//             selectCard[0].src = "../Image/Cat/backCat.jpg";
+//             selectCard[1].src = "../Image/Cat/backCat.jpg";
+//         }
+//         else{
+//             selectCard[0].flipFlag = 1;
+//             selectCard[1].flipFlag = 1;
+            
+//         }
+//         nowClickCount++; // nowClickCnt 1 증가
+//         console.log(nowClickCount);
+//         document.querySelector("#clickCnt").value = nowClickCount; // nowClickCnt를 html의 clickCnt에 반영
+//         cnt = 0; // cnt 초기화
+//     }
+// }
+
 function restart(){
-    window.reload(true);
+    window.location.reload();
 }

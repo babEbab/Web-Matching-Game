9ac436871414a8cb680f5ed545370d79cdf14047
babebab <dasomchoi52@gmail.com>
2020-11-14 21:28:57 +0900

    matching.js 기능 개발
    기록 표시 기능 개발, 로컬 스토리지 초기화 기능 개발

--- SourceCode/matching.js
+++ SourceCode/matching.js
@@ -1,5 +1,3 @@
-window.onload = start;
-
 var card = new Array(16); // 카드 배열
 
 var catImage = new Array("../Image/Cat/01.jpg", "../Image/Cat/01.jpg","../Image/Cat/02.jpg", "../Image/Cat/02.jpg","../Image/Cat/03.jpg", "../Image/Cat/03.jpg","../Image/Cat/04.jpg", "../Image/Cat/04.jpg","../Image/Cat/05.jpg", "../Image/Cat/05.jpg","../Image/Cat/06.jpg", "../Image/Cat/06.jpg","../Image/Cat/07.jpg", "../Image/Cat/07.jpg","../Image/Cat/08.jpg", "../Image/Cat/08.jpg");
@@ -8,10 +6,47 @@ var catImageFlag = new Array("0","0","0","0","0","0","0","0","0","0","0","0","0"
 var cnt = 0; // 플레이어가 카드를 뒤집은 횟수(2가 되면 서로 같은 카드인지 확인)
 var selectCard = new Array(2); // 사용자가 현재 선택한 카드 배열(1번에 최대 2장의 앞면을 보는 것이 가능)
 var nowClickCount = 0; // 플레이어가 카드를 뒤집은 총 횟수(2회 클릭 시에 1 증가)
+var recordList = new Array();
+
+window.onload = start;
 
 // 시작 함수
 function start(){
+    /* 로컬 스토리지에 등록된 값을 가져와서 최고 기록과 평균 기록을 구함 */
+    // recordList에 로컬 스토리지의 값 가져오기
+    var value = localStorage.getItem("localRecordList");
+    if (value != null) {
+        var storedStorage = JSON.parse(value);
+        storedStorage.forEach(function (Record) {
+            recordList.push(Record);
+        //    localStorage.setItem("localSonglist", JSON.stringify(recordList));
+        });
+        console.log(Array.isArray(storedStorage));
+        console.log(storedStorage);
+        console.log(value);
+    }
+    // recordList의 값을 통해 최고 기록과 평균 기록 구하기
+    if(recordList.length == 0){
+        var averCnt = 0;
+        var bestCnt = 0;
+    }
+    else{
+    var averCnt = 0;
+    var bestCnt = 99999;
+    for(var i = 0; i < recordList.length; i++){
+        averCnt += recordList[i].record;
+        if(recordList[i].record < bestCnt){
+            bestCnt = recordList[i].record;
+        }
+    }
+    averCnt = averCnt / recordList.length;
+    }
 
+    // averCnt와 bestCnt를 html의 averCnt와 bestCnt에 반영
+    document.querySelector("#averCnt").value = averCnt;
+    document.querySelector("#bestCnt").value = bestCnt;
+
+    /* 현재 플레이 보드 */
     var playTable = document.createElement("table");
     var playBoard = document.getElementById("playBoard");
     playTable.id = "playTable";
@@ -38,7 +73,7 @@ function start(){
     for(var i = 0; i < card.length; i++){
         card[i].flipFlag = 0;
     }
-    shuffle();
+    shuffle(); // 카드 섞기
 }
 
 // 카드 셔플 함수
@@ -56,6 +91,7 @@ function shuffle(){
     }
 }
 
+// 카드 뒤집기 함수
 function flip(obj){
     if(obj.flipFlag == 1) return;
     if(cnt != 0){
@@ -93,10 +129,22 @@ function flip(obj){
     }
 }
 
+// 짝맞추기 성공 함수
 function clear(){
     confirm("모든 짝을 맞추셨습니다. 게임을 재시작하세요!");
     // 현재 기록을 로컬 스토리지에 추가...
+    var recordObj = {
+        record: nowClickCount
+    }
+    recordList.push(recordObj);
+    localStorage.setItem("localRecordList", JSON.stringify(recordList));
+    nowClickCount = 0; // 현재 기록 초기화
     restart();
+}
+
+function clearRecordlist() {
+    localStorage.clear();
+    window.location.reload();
 }
 
 function restart(){
